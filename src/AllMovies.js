6b0ec2502e68d4b2b80f81a9cd56984790d28109
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-03-24 16:06:59 -0600

    Add movie rendering

--- src/AllMovies.js
+++ src/AllMovies.js
@@ -2,17 +2,15 @@ import React from 'react'
 import './AllMovies.css'
 import MovieTile from './MovieTile'
 
-const AllMovies = (props) => {
-  const movieInfo = this.props.map((movie) => {
+const AllMovies = (movies) => {
+  console.log(movies)
+  const movieInfo = movies.movies.map((movie) => {
     return (
       <MovieTile
         id={movie.id}
         key={movie.id}
         poster_path={movie.poster_path}
-        backdrop_path={movie.backdrop_path}
         title={movie.title}
-        average_rating={movie.average_rating}
-        release_date={movie.release_date}
       />
     )
   })
