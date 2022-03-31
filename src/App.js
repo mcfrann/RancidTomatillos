788e93e112235dd0f30125d90be9fbc37820fabd
Francesca McConnell <fmcconnell59@gmail.com>
2022-03-30 18:30:30 -0600

    Implemented ErrorMessage component in render

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AllMovies from './AllMovies'
 import MovieModule from './MovieModule'
 import fetchData from './api-calls'
 import icon from './popcorn.png'
+import ErrorMessage from './ErrorMessage'
 import { Route, NavLink } from 'react-router-dom'
 
 class App extends Component {
@@ -21,26 +22,7 @@ class App extends Component {
       .then(data => {
         this.setState({moviesKey: data.movies})
       })
-      .catch(error => this.setState({error: 'uh oh'}))
-  }
-
-  // showMovie = match => {
-  //   const newMovie = this.state.moviesKey.find(movie => movie.id === match)
-  //   this.setState({ moviesKey: newMovie })
-  // }
-
-  // showMovie = id => {
-  //   fetchData.getOneMovie(id)
-  //     .then(movie => this.setState({moviesKey: movie.movie}))
-  //     .catch(error => this.setState({error: 'unable to find movie'}))
-  // }
-
-  returnHome = () => {
-    fetchData.getAllMovies()
-      .then(data => {
-        this.setState({moviesKey: data.movies})
-      })
-      .catch(error => this.setState({error: 'uh oh'}))
+      .catch(error => this.setState({error: error}))
   }
 
   render() {
@@ -52,24 +34,16 @@ class App extends Component {
             </NavLink>
           </header>
             <main>
-              {this.state.error && <h2>Uh oh! Cannot access server.</h2>}
+              {this.state.error && <ErrorMessage error={this.state.error}/>}
               {this.state.moviesKey.length > 1 &&
               <Route path="/" exact render={ () => <AllMovies
                 movies={this.state.moviesKey}/>} />}
-              {/* {this.state.moviesKey.length < 50 &&
-                <h2>
-                  {' '}
-                  <img src={icon} alt='popcorn-icon' id='popcornIcon' />
-                </h2>} */}
               <Route path="/:id" render={ ({ match }) => {
-                // const currentMovie = this.state.moviesKey.find(movie => movie.id === parseInt(match.params.id))
                 const id = parseInt(match.params.id)
                 return <MovieModule
-                  // movieID={movieID}
                   showMovie={this.showMovie}
                   returnHome={this.returnHome}
                   id={id}
-                  // currentMovie={currentMovie}
                   />}}
                   />
             </main>
@@ -80,22 +54,3 @@ class App extends Component {
 
 
 export default App
-
-
- {/* <AllMovies
-                  movies={this.state.moviesKey}
-                  showMovie={this.showMovie}
-                />
-              ) : this.state.moviesKey.length < 50 ? (
-                <h2>
-                  {' '}
-                  <img src={icon} alt='popcorn-icon' id='popcornIcon' />
-                </h2>
-              ) : (
-                <MovieModule
-                  showMovie={this.showMovie}
-                  returnHome={this.returnHome}
-                  currentMovie={this.state.moviesKey}
-                  displayNumber={this.displayNumber}
-                />
-              )} */}
