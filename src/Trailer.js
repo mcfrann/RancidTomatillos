4fe21731482e3a8577ade0e031f127fe489338b7
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-02 15:35:48 -0600

    refactor trailer

--- src/Trailer.js
+++ src/Trailer.js
@@ -4,11 +4,8 @@ import './Trailer.css'
 const Trailer = ({ url }) => {
 	return (
 		<div className='trailer'>
-<<<<<<< HEAD
 			<h2>TRAILER TIME</h2>
-=======
 			console.log(url)
->>>>>>> dcd3dded69f88faab1c39522da74ecbe158a5923
 			<iframe title='watch-trailer' width='420' height='315' src={url}></iframe>
 		</div>
 	)
